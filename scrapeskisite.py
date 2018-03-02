998aa446e37adbd53facd8f4d5e550c164bc50e6
DarekRepos <duda.dariusz.dd@gmail.com>
2018-03-02 19:20:24 +0100

    Scrape all ski jumping events

--- scrapeskisite.py
+++ scrapeskisite.py
@@ -2,12 +2,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://http://www.skokinarciarskie.pl/'
+FIELDS = ('programgl_czas', 'programgl_impreza', 'programgl_wydarzenie')
+stories = []
+
+url = 'http://www.skokinarciarskie.pl/'
 ss = requests.Session()
 res = ss.get(url)
 soup = BeautifulSoup(res.content, 'html5lib')
 
-# locate the area row
-tr = soup.find(attrs={'class': 'program_czas'})
-area = tr.text  # extract the data
-print(area)
+
+def interest_tags(tag):
+    if tag.name == "div":
+        classes = tag.get("class", [])
+        return "programgl_data" in classes or \
+               "programgl_pole1" in classes or \
+               "programgl_pole2" in classes
+
+
+for czasy in soup.find_all(interest_tags):
+
+    if czasy.attrs == {'class': ['programgl_data']}:
+        print(czasy.get_text(), end=' ')
+
+    stories = czasy.find_all('div', class_={FIELDS})
+    for ss in stories:
+        print(ss.get_text(), end=' ')
+
+    print("")

8d663f37536cafea183b5d3fe7f90399bc16df02
Dareczek <dareczek06@gmail.com>
2023-05-18 09:23:09 +0200

    Fixed linter issues

--- src/scrapeskisite.py
+++ src/scrapeskisite.py
@@ -21,25 +21,27 @@ def main():
     res = ss.get(
         url,
         headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"
+            "User-Agent": "Mozilla/5.0 " +
+            "(Windows NT 10.0; Win64; x64) " +
+            "AppleWebKit/537.36 (KHTML, like Gecko) " +
+            "Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"
         },
         cookies={"CONSENT": "YES+"},
     )
     soup = BeautifulSoup(res.content, "html.parser")
 
-    file = open("SkiEvents.txt", "a+")
+    with open("SkiEvents.txt", "a+",  encoding="utf-8") as file:
 
-    for days in soup.find_all(event_tags):
-        if days.attrs == {"class": ["programgl_data"]}:
-            file.write(days.get_text() + " ")
+        for days in soup.find_all(event_tags):
+            if days.attrs == {"class": ["programgl_data"]}:
+                file.write(days.get_text() + " ")
 
-        hours = days.find_all("div", class_={FIELDS})
-        for event in hours:
-            file.write(event.get_text() + "|")
+            hours = days.find_all("div", class_={FIELDS})
+            for event in hours:
+                file.write(event.get_text() + "|")
 
-        file.write("\n")
-
-    print("Dane zapisano w pliku SkiEvents.txt")
+            file.write("\n")
+        print("Dane zapisano w pliku SkiEvents.txt")
 
 
 if __name__ == "__main__":

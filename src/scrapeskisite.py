8551316191d2dfdd8c5498159ea116e90813c73c
Dareczek <dareczek06@gmail.com>
2023-05-18 10:41:40 +0200

    resolved #9 , resolved #10
    - added documentation headers
    - added comments
    - refactor variable names
    - added features description
    - since python3 there is no need to define utf-8 encoding

--- src/scrapeskisite.py
+++ src/scrapeskisite.py
@@ -1,24 +1,57 @@
-# _*_coding:utf-8_*_
+"""
+Program that scrape the ski website to get all Ski World Cup Events
+
+There must be ski events to see the result data
+
+Example:
+
+python3 scrapeskisite.py
+
+@Author: Darek Duda
+@Date: 18.05.2023
+@Credit: DarekRepos
+@Links: https://github.com/DarekRepos
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
 
 def event_tags(tag):
+    """It is function that search for every page elements
+    and return children elements node that contain classes
+    which event data are inside
+
+
+    Args:
+        tag (string): It is a page element that contain event data ex: "div"
+
+    Returns:
+        html node : return page element node for finding events
+    """
+
+    event_date_node = "programgl_data"
+    word_cup_event_node_1 = "programgl_pole1"
+    word_cup_event_node_2 = "programgl_pole2"
+
     if tag.name == "div":
         classes = tag.get("class", [])
         return (
-            "programgl_data" in classes
-            or "programgl_pole1" in classes
-            or "programgl_pole2" in classes
+            event_date_node in classes
+            or word_cup_event_node_1 in classes
+            or word_cup_event_node_2 in classes
         )
 
 
 def main():
-    FIELDS = ("programgl_czas", "programgl_impreza", "programgl_wydarzenie")
+    """
+    Main function that program are executed
+    """
+    fields = ("programgl_czas", "programgl_impreza", "programgl_wydarzenie")
 
     url = "http://www.skokinarciarskie.pl/"
-    ss = requests.Session()
-    res = ss.get(
+    session = requests.Session()
+    response = session.get(
         url,
         headers={
             "User-Agent": "Mozilla/5.0 " +
@@ -28,16 +61,18 @@ def main():
         },
         cookies={"CONSENT": "YES+"},
     )
-    soup = BeautifulSoup(res.content, "html.parser")
+    soup = BeautifulSoup(response.content, "html.parser")
 
     with open("SkiEvents.txt", "a+",  encoding="utf-8") as file:
-
-        for days in soup.find_all(event_tags):
-            if days.attrs == {"class": ["programgl_data"]}:
-                file.write(days.get_text() + " ")
-
-            hours = days.find_all("div", class_={FIELDS})
-            for event in hours:
+        # Iterate for every node that contain a day of the event
+        # or all events in the Ski World Cup 
+        for node in soup.find_all(event_tags):
+            # write an event date to the file
+            if node.attrs == {"class": ["programgl_data"]}:
+                file.write(node.get_text() + " ")
+            # write all events to  the file for the given date
+            events = node.find_all("div", class_={fields})
+            for event in events:
                 file.write(event.get_text() + "|")
 
             file.write("\n")
